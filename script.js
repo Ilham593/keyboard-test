03bf0c5e999d892f89b1f422d579aa6ae0407efd
Ilhambkl593 <sosham127@gmail.com>
2024-09-17 21:40:47 +0700

    1.1

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const label_clear = document.getElementById('label-clear')
 label_clear.addEventListener('click', function(){
   input.value = ''
 })
+
 window.addEventListener('keydown', function(e) {
   huruf.forEach(button => {
     if (button.textContent.toLowerCase() === e.key.toLowerCase()) {
@@ -26,6 +27,29 @@ window.addEventListener('keydown', function(e) {
   });
 });
 
+
+window.addEventListener('keyup', function(e) {
+  huruf.forEach(button => {
+    if (button.textContent.toLowerCase() === e.key.toLowerCase()) {
+
+      // Jika tombol yang ditekan cocok dengan teks di tombol virtual (case-insensitive)
+      handle(button); // Tambahkan kelas 'aktif' ke tombol yang sesuai
+      input.value += button.textContent;
+    } else if (e.key === "Escape") {
+      console.log("Escape key pressed");
+      handleSpecialButton('Escape');
+    } else if (e.key === "Control") {
+      console.log("Control key pressed");
+      handleSpecialButton('Control');
+    } else if (e.key === "Enter") {
+      console.log("Enter key pressed");
+      handleSpecialButton('Enter');
+    } 
+  });
+});
+
+
+
 function handle(button) {
   button.classList.toggle('aktif'); // Tambahkan efek pada tombol huruf/angka
 }
@@ -36,3 +60,18 @@ function handleSpecialButton(keyName) {
     specialButton.classList.add('spesial'); // Tambahkan efek pada tombol khusus
   }
 }
+
+window.addEventListener('DOMContentLoaded', function(){
+  let width = window.innerWidth
+  let height = window.innerHeight
+
+  console.log(width , height)
+  const container =  document.getElementById('container')
+  const warning = document.querySelector('.warning-none')
+  if(width < 800){
+    alert(' nampaknya anda membuka di layar hp,web ini khusus layar desktop')
+    container.style.display = 'none'
+    warning.style.display = 'block'
+
+  }
+})
